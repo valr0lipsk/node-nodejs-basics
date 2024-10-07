149bd949eb5029289dbfb9e8aa39686d591b06da
Valery Lipskaya <valery.lipskaya03@gmail.com>
2024-10-07 08:40:39 +0200

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
+import { createReadStream } from "fs";
+import path from "path";
+import { createHash } from "crypto";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  return new Promise((resolve, reject) => {
+    const hash = createHash("sha256");
+    const stream = createReadStream(filePath);
+
+    stream.on("error", (error) => {
+      reject(error);
+    });
+
+    stream.on("data", (chunk) => {
+      hash.update(chunk);
+    });
+
+    stream.on("end", () => {
+      const fileHash = hash.digest("hex");
+      console.log(fileHash);
+      resolve(fileHash);
+    });
+  });
 };
 
 await calculateHash();
