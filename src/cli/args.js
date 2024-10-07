1fc06b2c59b5f176d224494251fe5b14635cf1eb
Valery Lipskaya <valery.lipskaya03@gmail.com>
2024-10-07 08:25:44 +0200

    feat: cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const result = args
+    .reduce((acc, arg, index) => {
+      if (arg.startsWith("--")) {
+        acc.push(`${arg.slice(2)} is ${args[index + 1]}`);
+      }
+      return acc;
+    }, [])
+    .join(", ");
+  console.log(result);
 };
 
 parseArgs();
